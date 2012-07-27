4a160cc6f40fe9657878c3d01dc3a24737b1b8d6
Christian Vermeulen <info@christianvermeulen.net>
2012-07-27 13:46:27 +0200

    updated homepage

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,10 @@
 <h2>Welkom bij Coderdojo Rotterdam</h2>
 <p>CoderDojo Rotterdam verzorgt gratis lessen / workshops om kinderen van 7 - 18 jaar te leren programmeren. Wij organiseren regelmatig evenementen waar kinderen, met hun ouders, kunnen samenkomen om te werken aan websites, games, apps en software. Het hele project wordt door vrijwilligers opgezet!</p>
+<p class="center">
+	<a href="/vrijwilligers-gezocht" class="small radius green button">Meld je aan als vrijwilliger</a>
+	<a href='http://eepurl.com/j30pT' class="small radius green button">Schrijf je vast in voor de nieuwsbrief</a>
+</p>
 <p>Een voorbeeld van een game die gemaakt is door een van de kinderen is een pizzavariant op de game Space Invaders. Harry is nog maar 12 jaar oud en wist deze game helemaal zelf in elkaar te zetten tijdens de CoderDojo van Cork.</p>
-<p style="text-align:center;"><iframe width="300" height="200" src="http://www.youtube.com/embed/ImJD-nDxnc4" frameborder="0" allowfullscreen></iframe></p>
-<p>Als je graag wilt mee helpen met het organiseren van een Dojo kun je je ook opgeven als <a href="/vrijwilligers-gezocht">vrijwilliger</a>.</p>
-<p style="text-align:center;">-> <a href='http://eepurl.com/j30pT'>Schrijf je vast in voor de nieuwsbrief! <-</a></p>
+<div class="flex-video widescreen">
+	<iframe width="560" height="315" src="http://www.youtube.com/embed/ImJD-nDxnc4" frameborder="0" allowfullscreen></iframe>
+</div>
