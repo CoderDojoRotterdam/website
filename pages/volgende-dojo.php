3e0cde1b15fa1ea0888898fd02d252891cb7a218
Christian Vermeulen <info@christianvermeulen.net>
2013-09-16 20:14:09 +0200

    added event brite widget

--- pages/volgende-dojo.php
+++ pages/volgende-dojo.php
@@ -1,6 +1,9 @@
-<h2>De volgende dojo</h2>
-<p>Momenteel zijn we nog druk bezig om de eerste Dojo op te zetten. Zodra we meer informatie hebben zal deze hier bekend gemaakt worden!</p>
+<h2>Coder Dojo Rotterdam #1</h2>
+<p>De eerste Coder Dojo van Rotterdam zal plaatsvinden op zaterdag 26 oktober 2013!</p>
+<p>We zullen zo snel mogelijk meer informatie geven over deze dojo. Hieronder kun je vanaf maandag 23 september je ticket bestellen</p>
+<div style="width:100%; text-align:left;" ><iframe  src="https://www.eventbrite.nl/tickets-external?eid=8352086315&ref=etckt&v=2" frameborder="0" height="214" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.nl/r/etckt">Online tickets verkopen</a><span style="color:#ddd;"> voor </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="https://coderdojorotterdam.eventbrite.nl?ref=etckt">Coder Dojo Rotterdam #1</a> <span style="color:#ddd;">mogelijk gemaakt door</span> <a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.nl?ref=etckt">Eventbrite</a></div></div>
+
 <p class="center">
-	<a href="/vrijwilligers-gezocht" class="small radius green button">Meld je aan als vrijwilliger</a>
-	<a href='http://eepurl.com/j30pT' class="small radius green button">Schrijf je vast in voor de nieuwsbrief</a>
+    <a href="/vrijwilligers-gezocht" class="small radius green button">Meld je aan als vrijwilliger</a>
+    <a href='http://eepurl.com/j30pT' class="small radius green button">Schrijf je vast in voor de nieuwsbrief</a>
 </p>
